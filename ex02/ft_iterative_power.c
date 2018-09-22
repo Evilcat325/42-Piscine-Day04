26b3787bf580af0210f288d480100eba2404284d
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-22 01:53:46 -0700

    cache base

--- ex02/ft_iterative_power.c
+++ ex02/ft_iterative_power.c
@@ -6,19 +6,22 @@
 /*   By: seli <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/22 01:48:03 by seli              #+#    #+#             */
-/*   Updated: 2018/09/22 01:52:05 by seli             ###   ########.fr       */
+/*   Updated: 2018/09/22 01:53:30 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_iterative_power(int nb, int power)
 {
+	int base;
+
+	base = nb;
 	if (power < 0)
 		return (0);
 	if (power == 0)
 		return (1);
 	while (power > 1)
 	{
-		nb *= nb;
+		nb *= base;
 		power--;
 	}
 	return (nb);
