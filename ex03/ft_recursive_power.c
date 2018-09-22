2c59c1d750a60f5158a43bb9cbd5b6c45f89e484
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-22 02:07:44 -0700

    check overflow

--- ex03/ft_recursive_power.c
+++ ex03/ft_recursive_power.c
@@ -6,15 +6,24 @@
 /*   By: seli <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/22 01:54:34 by seli              #+#    #+#             */
-/*   Updated: 2018/09/22 01:56:37 by seli             ###   ########.fr       */
+/*   Updated: 2018/09/22 02:07:32 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_power(int nb, int power)
 {
+	int result;
+	int prev_result;
+
+	if (nb == 1)
+		return (1);
 	if (power < 0)
 		return (0);
 	if (power == 0)
 		return (1);
-	return (nb * ft_recursive_power(nb, power - 1));
+	prev_result = ft_recursive_power(nb, power - 1);
+	result = prev_result * nb;
+	if (result / nb != prev_result)
+		return (-1);
+	return (result);
 }
