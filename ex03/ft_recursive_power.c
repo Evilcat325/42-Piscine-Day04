69409284024a3e85c4f2627ec7d33f0e734b88f8
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-22 02:11:52 -0700

    update exit logic

--- ex03/ft_recursive_power.c
+++ ex03/ft_recursive_power.c
@@ -6,7 +6,7 @@
 /*   By: seli <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/22 01:54:34 by seli              #+#    #+#             */
-/*   Updated: 2018/09/22 02:07:32 by seli             ###   ########.fr       */
+/*   Updated: 2018/09/22 02:11:28 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ int	ft_recursive_power(int nb, int power)
 	if (power == 0)
 		return (1);
 	prev_result = ft_recursive_power(nb, power - 1);
+	if (prev_result == -1)
+		return (-1);
 	result = prev_result * nb;
 	if (result / nb != prev_result)
 		return (-1);
