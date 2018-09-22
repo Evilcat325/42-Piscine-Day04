e4c6f9c6e70d2de2319b6153f7bb6b52fe88c6d4
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-22 01:23:37 -0700

    check overflow

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -6,15 +6,20 @@
 /*   By: seli <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/22 00:44:57 by seli              #+#    #+#             */
-/*   Updated: 2018/09/22 01:01:57 by seli             ###   ########.fr       */
+/*   Updated: 2018/09/22 01:23:22 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_iterative_factorial(int nb)
 {
+	int result;
+
 	if (nb == 1)
 		return (1);
 	if (nb <= 0)
 		return (0);
-	return (nb * ft_iterative_factorial(nb - 1));
+	result = ft_iterative_factorial(nb - 1);
+	if (result * nb < result)
+		return (0);
+	return (nb * nb);
 }
