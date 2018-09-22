615f215078b8029f4621ceed75f879fb4b1e571d
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-22 01:43:06 -0700

    fix iterative logic

--- ex01/ft_recursive_factorial.c
+++ ex01/ft_recursive_factorial.c
@@ -1,32 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seli <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/09/22 00:44:57 by seli              #+#    #+#             */
-/*   Updated: 2018/09/22 01:42:43 by seli             ###   ########.fr       */
+/*   Created: 2018/09/22 01:34:01 by seli              #+#    #+#             */
+/*   Updated: 2018/09/22 01:41:01 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_iterative_factorial(int nb)
+int	ft_recursive_factorial(int nb)
 {
 	int result;
 	int next_result;
 
-	result = 1;
-	if (nb <= 0)
-		return (0);
 	if (nb == 1 || nb == 0)
 		return (1);
-	while (nb > 1)
-	{
-		result *= nb;
-		nb--;
-		next_result = result * nb;
-		if (next_result / nb != result)
-			return (0);
-	}
+	if (nb <= 0)
+		return (0);
+	result = ft_recursive_factorial(nb - 1);
+	next_result = result * nb;
+	if (next_result / nb != result)
+		return (0);
 	return (next_result);
 }
